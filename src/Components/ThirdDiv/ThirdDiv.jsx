dbf06be1393f39ba035aebadde6555ea48b05520
Anandhu Ramakrishnan <anandhurk1888@gmail.com>
2023-07-23 15:37:28 +0530

    contact session

--- src/Components/ThirdDiv/ThirdDiv.jsx
+++ src/Components/ThirdDiv/ThirdDiv.jsx
@@ -10,9 +10,21 @@ const ThirdDiv = () => {
         <div className="card2"></div>
         <div className="card3"></div>
         <div className="card4"></div>
+        
       </div>
-       
+      <div className="project_names">
+        <div className="name1"> project1</div>
+        <div className="name2"> project1</div>
+        <div className="name3"> project1</div>
+        <div className="name4"> project1</div>
+      </div>
      
+     <div className="textfield">
+      <div className="nb">you can give direct msg to me here. i will contact you as soon as possible. </div>
+      
+        <textarea name="msg" id="txtarea" cols="30" rows="10"></textarea>
+    
+     </div>
     </div>
   );
 };
