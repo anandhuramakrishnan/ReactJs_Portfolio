31c5918565d19d60b3846ba65ac8fb92738c797a
Anandhu Ramakrishnan <anandhurk1888@gmail.com>
2023-07-23 18:37:11 +0530

    contact + btn

--- src/Components/ThirdDiv/ThirdDiv.jsx
+++ src/Components/ThirdDiv/ThirdDiv.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../ThirdDiv/ThirdDiv.css";
 const ThirdDiv = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+  const clickhandler = () => {
+    // Implement your logic to send the message or perform any desired action here
+    console.log("Phone Number:", phoneNumber);
+    console.log("Message:", message);
+    // Clear the input fields after sending
+    setPhoneNumber("");
+    setMessage("");
+  };
   return (
     <div className="thirddiv">
       <p className="head3">PROJECTS </p>
@@ -21,8 +31,16 @@ const ThirdDiv = () => {
      
      <div className="textfield">
       <div className="nb">you can give direct msg to me here. i will contact you as soon as possible. </div>
-      
-        <textarea name="msg" id="txtarea" cols="30" rows="10"></textarea>
+      <div className="phonenumber">
+        <label htmlFor="label1 :" className="label1">Phone-no :</label>
+      <textarea name="phone" id="phone" cols="1 " rows="1"   placeholder="Enter phone number" maxlength="15" ></textarea>
+      </div>
+      <div className="msg">
+      <label htmlFor="label2 :" className="label2">Message :</label>
+        <textarea name="msg" id="textarea" cols="30" rows="10"></textarea>
+        
+      </div>
+      <button className="btn" onClick={clickhandler}> submit </button>
     
      </div>
     </div>
